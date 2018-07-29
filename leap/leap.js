1674a4de62e10fed7cd1150b70730a35845b67e4
Sebastian Bethell <sebastianbethell@Sebastians-MacBook-Pro.local>
2018-07-28 17:45:28 -0700

    three simple if statements

--- leap/leap.js
+++ leap/leap.js
@@ -1,13 +1,20 @@
-var Year = function () {
-//
-// YOUR CODE GOES HERE
-//
+var Year = function (yearInput) {
+  this.value = yearInput;
+  return yearInput;
 };
 
 Year.prototype.isLeap = function () {
-//
-// YOUR CODE GOES HERE
-//
+  if (this.value % 4 == 0) {
+    if (this.value % 100 == 0) {
+      if (this.value % 400 == 0) {
+        return true;
+      }
+        return false;
+    }
+    return true;
+  }
+  return false;
 };
+
 
 module.exports = Year;
